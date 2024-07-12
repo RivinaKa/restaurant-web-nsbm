69798c3565538688413ab1430aa9a78b65a733d9
Rivinaka Dulneth <128132994+RivinaKa@users.noreply.github.com>
2024-07-12 20:22:17 +0530

    Add files via upload

---
+++ components/footer.php
@@ -0,0 +1,45 @@
+<section class="footer">
+
+    <div class="box-container">
+
+        <div class="box">
+            <h3>locations</h3>
+            <a href="#">Colombo</a>
+            <a href="#">Gampaha</a>
+            <a href="#">galle</a>
+            <a href="#">jaffna</a>
+            <a href="#">hambanthota</a>
+        </div>
+
+        <div class="box">
+            <h3>quick links</h3>
+            <a href="#">home</a>
+            <a href="#">dishes</a>
+            <a href="#">about</a>
+            <a href="#">menu</a>
+            <a href="#">reivew</a>
+            <a href="#">order</a>
+        </div>
+
+        <div class="box">
+            <h3>contact info</h3>
+            <a href="#">+94-123-456-789</a>
+            <a href="#">+94-123-456-789</a>
+            <a href="#">kamaalrestornet@gmail.com</a>
+            <a href="#">info.snapeats@gmail.com</a>
+            <a href="#">Colombo, Sri lanka. </a>
+        </div>
+
+        <div class="box">
+            <h3>follow us</h3>
+            <a href="#">facebook</a>
+            <a href="#">twitter</a>
+            <a href="#">instagram</a>
+            <a href="#">linkedin</a>
+        </div>
+
+    </div>
+
+    <div class="credit"> copyright @ 2024 by <span>group 05</span> </div>
+
+</section>
---
+++ js/script.js
@@ -0,0 +1,48 @@
+navbar = document.querySelector('.header .flex .navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+   navbar.classList.toggle('active');
+   profile.classList.remove('active');
+}
+
+profile = document.querySelector('.header .flex .profile');
+
+document.querySelector('#user-btn').onclick = () =>{
+   profile.classList.toggle('active');
+   navbar.classList.remove('active');
+}
+
+window.onscroll = () =>{
+   navbar.classList.remove('active');
+   profile.classList.remove('active');
+}
+
+var swiper = new Swiper(".home-slider", {
+   spaceBetween: 30,
+   centeredSlides: true,
+   autoplay: {
+     delay: 7500,
+     disableOnInteraction: false,
+   },
+   pagination: {
+     el: ".swiper-pagination",
+     clickable: true,
+   },
+   loop:true,
+ });
+
+function loader(){
+   document.querySelector('.loader').style.display = 'none';
+}
+
+function fadeOut(){
+   setInterval(loader, 2000);
+}
+
+window.onload = fadeOut;
+
+document.querySelectorAll('input[type="number"]').forEach(numberInput => {
+   numberInput.oninput = () =>{
+      if(numberInput.value.length > numberInput.maxLength) numberInput.value = numberInput.value.slice(0, numberInput.maxLength);
+   };
+});
